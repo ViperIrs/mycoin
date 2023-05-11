c0bcbb345e805fca7d024d5d61be2fc827029b36
Igor Ribeiro Scarlassara <igor.1054238@alunos.unisagrado.edu.br>
2023-05-10 23:01:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mycoin5/TelaConceitos.java
@@ -0,0 +1,15 @@
+package com.example.mycoin5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaConceitos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_conceitos);
+        getSupportActionBar().hide();
+    }
+}
