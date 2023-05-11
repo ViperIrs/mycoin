4a8b019683f77d18d697cc48d09c296fdbcc07c2
Igor <igorscarla@gmail.com>
2023-05-11 19:23:37 -0300

    Mudancas

--- app/src/main/java/com/example/mycoin5/TelaConceitos.java
+++ app/src/main/java/com/example/mycoin5/TelaConceitos.java
@@ -2,14 +2,27 @@ package com.example.mycoin5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TelaConceitos extends AppCompatActivity {
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_conceitos);
         getSupportActionBar().hide();
+        ImageView logo = findViewById(R.id.logo);
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(TelaConceitos.this, MainActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
