4f45a00f94fd2584afde1abfe0c212637298d3dc
Igor Ribeiro Scarlassara <igor.1054238@alunos.unisagrado.edu.br>
2023-05-20 17:18:55 -0300

    att tela passos

--- app/src/main/java/com/example/mycoin5/TelaConceitos.java
+++ app/src/main/java/com/example/mycoin5/TelaConceitos.java
@@ -1,69 +1,56 @@
 package com.example.mycoin5;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
 public class TelaConceitos extends AppCompatActivity {
-    ImageView logo;
     ImageView btnReceitas;
     ImageView btnDespesas;
-    ImageView btnOrcamento;
+    ImageView btnOrca;
     ImageView btnPlanejamento;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_conceitos);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         ImageView logo = findViewById(R.id.logo);
         btnReceitas = findViewById(R.id.btnReceitas);
         btnDespesas = findViewById(R.id.btnDespesas);
-        btnOrcamento = findViewById(R.id.btnOrcamento);
+        btnOrca = findViewById(R.id.btnOrca);
         btnPlanejamento = findViewById(R.id.btnPlanejamento);
 
-        btnDespesas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in = new Intent(TelaConceitos.this, TelaDespesas.class);
-                startActivity(in);
-            }
+        btnDespesas.setOnClickListener(view -> {
+            Intent in = new Intent(TelaConceitos.this, TelaDespesas.class);
+            startActivity(in);
         });
 
-        btnReceitas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in = new Intent(TelaConceitos.this, TelaReceitas.class);
-                startActivity(in);
-            }
+        btnReceitas.setOnClickListener(view -> {
+            Intent in = new Intent(TelaConceitos.this, TelaReceitas.class);
+            startActivity(in);
         });
 
-        btnPlanejamento.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in = new Intent(TelaConceitos.this, TelaPlanejamento.class);
-                startActivity(in);
-            }
+        btnPlanejamento.setOnClickListener(view -> {
+            Intent in = new Intent(TelaConceitos.this, TelaPlanejamento.class);
+            startActivity(in);
         });
 
-        logo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in = new Intent(TelaConceitos.this, MainActivity.class);
-                startActivity(in);
-            }
+        logo.setOnClickListener(view -> {
+            Intent in = new Intent(TelaConceitos.this, MainActivity.class);
+            startActivity(in);
         });
 
-        btnOrcamento.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in = new Intent(TelaConceitos.this, TelaOrcamento.class);
-                startActivity(in);
-            }
+        btnOrca.setOnClickListener(view -> {
+            Intent in = new Intent(TelaConceitos.this, TelaOrcamento.class);
+            startActivity(in);
         });
 
     }
