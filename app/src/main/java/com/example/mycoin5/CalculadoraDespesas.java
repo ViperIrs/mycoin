0b1e44942b1b2c8d7d38d63979e89219c9adb2c9
thiagodcamill0 <thiagodcamillo@gmail.com>
2023-05-20 20:02:53 -0300

    calculadora despesas

--- app/src/main/java/com/example/mycoin5/CalculadoraDespesas.java
+++ app/src/main/java/com/example/mycoin5/CalculadoraDespesas.java
@@ -3,12 +3,23 @@ package com.example.mycoin5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CalculadoraDespesas extends AppCompatActivity {
+
+    EditText = edittext;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora_despesas);
+
+
+        edittext = findViewById(R.id.EditTextDespesas);
+
+
     }
 }
